1c374eda10415076aead7b0aaef2f37bdf7a474b
marsalan828 <marsalan828@gmail.com>
2023-12-22 16:25:24 +0500

    added model

--- database/migrations/2023_12_22_093742_create_posts_table.php
+++ database/migrations/2023_12_22_093742_create_posts_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('is_admin',['admin','user']);
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('content');
+            $table->enum('status',['approved', 'unapproved']);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
